4d326367296fdd98baf81fb399071b48d68ebce4
fabianwolski <fabianwolski03@gmail.com>
2025-02-26 15:42:15 +0000

    Feat: Echo and Exit 0

--- cmd/myshell/main.go
+++ cmd/myshell/main.go
@@ -7,25 +7,66 @@ import (
 	"strings"
 )
 
-var _ = fmt.Fprint
+var builtIns = []string{"exit", "exit 0", "echo"}
+
+func executeCommand(input string) bool {
+
+	if len(input) == 0 {
+		return true
+	}
+
+	if input == "exit 0" {
+		os.Exit(0)
+	}
+
+	var parts = strings.Split(input, " ")
+	var cmd = parts[0]
+	var argument = parts[1:]
+
+	isBuiltIn := false
+	for _, builtIn := range builtIns {
+		if cmd == builtIn {
+			isBuiltIn = true
+			break
+		}
+	}
+
+	if !isBuiltIn {
+		fmt.Printf("%s: command not found\n", cmd)
+		return true
+	}
+
+	switch cmd {
+	case "exit":
+		os.Exit(0)
+	case "echo":
+		if len(argument) > 0 {
+			fmt.Println(strings.Join(argument, " "))
+		} else {
+			fmt.Println()
+		}
+		return true
+	}
+
+	return true
+}
 
 func main() {
-	fmt.Fprint(os.Stdout, "$ ")
+	reader := bufio.NewReader(os.Stdin)
 
 	for {
-
-		command, err := bufio.NewReader(os.Stdin).ReadString('\n')
+		fmt.Print("$ ")
+		command, err := reader.ReadString('\n')
 
 		if err != nil {
 			fmt.Println("Error reading input:", err)
 			os.Exit(1)
 		}
+
 		command = strings.TrimSpace(command)
-		fmt.Fprintln(os.Stdout, command+": command not found")
 
-		main()
+		executeCommand(command)
+
 	}
-
-	// Wait for user input
 
 }
