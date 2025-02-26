33eb743bad4bb38c918687f30eb5d2840287d129
fabianwolski <fabianwolski03@gmail.com>
2025-02-26 13:36:05 +0000

    Feature: REPL

--- cmd/myshell/main.go
+++ cmd/myshell/main.go
@@ -4,12 +4,28 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"strings"
 )
 
 var _ = fmt.Fprint
 
 func main() {
 	fmt.Fprint(os.Stdout, "$ ")
+
+	for {
+
+		command, err := bufio.NewReader(os.Stdin).ReadString('\n')
+
+		if err != nil {
+			fmt.Println("Error reading input:", err)
+			os.Exit(1)
+		}
+		command = strings.TrimSpace(command)
+		fmt.Fprintln(os.Stdout, command+": command not found")
+
+		main()
+	}
+
 	// Wait for user input
-	bufio.NewReader(os.Stdin).ReadString('\n')
+
 }
